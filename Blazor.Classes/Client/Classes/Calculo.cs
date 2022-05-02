67f45b2539781246447e8dd3648788061710f5e2
immerfallen <marodemelo10@gmail.com>
2022-05-02 14:33:10 -0300

    Add project files.

---
+++ Blazor.Classes/Client/Classes/Calculo.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.Classes.Client.Classes
+{
+    public class Calculo
+    {
+        public static int Somar(int n1, int n2) => n1 + n2;
+        public static int Subtrair(int n1, int n2) => n1 - n2;
+        public static int Multiplicar(int n1, int n2) => n1 * n2;
+    }
+}
---
+++ Blazor.Classes/Shared/Entities/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Classes.Shared.Entities
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
