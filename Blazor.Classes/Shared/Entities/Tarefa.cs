25068ca534976ebf695b3aacc491b1b498e7e3a8
immerfallen <marodemelo10@gmail.com>
2022-05-09 17:59:50 -0300

    wip

---
+++ Blazor.Classes/Shared/Entities/Tarefa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Classes.Shared.Entities
+{
+    public class Tarefa
+    {
+        public Guid Id { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public bool Concluida { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+    }
+}
